4fe7d398ca7979a55696636a2e004b2f4ee2152b
deo <deoninja@gmail.com>
2024-02-25 22:35:01 +0800

    pages

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <div>Hello Nuxt 3!!!</div>
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+            <NuxtLink to="/events/myprofile">Profile</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/events/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+    // const route = useRoute();
+  // console.log(route.params.id);
+  const nuxt = useNuxtApp();
+  console.log(nuxt);
+</script>
+
+<template>
+  <div>event id: {{  $route.params.id  }}</div>
+</template>
